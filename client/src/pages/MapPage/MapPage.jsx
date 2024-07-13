1df00bd0fdeb1b4c12d090843ff837df65bd925f
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-13 18:14:39 -0400

    Comment map page to-do.

---
+++ client/src/pages/MapPage/MapPage.jsx
@@ -0,0 +1,5 @@
+export default function MapPaage () {
+    <>
+        {/* Needs to be full screen react-leaflet map with markers containing location descriptions and links to the page. */}
+    </>
+}
