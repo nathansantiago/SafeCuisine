84121d9b8038120ec5293875eb24fc07fe26e714
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-13 18:12:25 -0400

    Style list view page.

--- client/src/components/RestaurantListItem/RestaurantListItem.jsx
+++ client/src/components/RestaurantListItem/RestaurantListItem.jsx
@@ -2,10 +2,13 @@ import "./RestaurantListItem.scss"
 
 export default function RestaurauntListItem () {
     return (
+        <>
         <div className="restauraunt-container">
             <div className="rest-info-container">
                 <h1>Venable Rotisserie Bistro</h1>
-                <h3>200 N Greensboro St A-18, Carrboro, NC 27510</h3>
+                <h3>
+                    <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">200 N Greensboro St A-18, Carrboro, NC 27510</a>
+                </h3>
                 <h3>1.32 miles</h3>
             </div>
             <div className="rest-ratings-container">
@@ -13,5 +16,19 @@ export default function RestaurauntListItem () {
                 <h4>15 Ratings</h4>
             </div>
         </div>
+        <div className="restauraunt-container">
+            <div className="rest-info-container">
+                <h1>Buns</h1>
+                <h3>
+                    <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">200 N Greensboro St A-18, Carrboro, NC 27510</a>
+                </h3>
+                <h3>0.88 miles</h3>
+            </div>
+            <div className="rest-ratings-container">
+                <h1>4.4</h1>
+                <h4>10 Ratings</h4>
+            </div>
+        </div>
+        </>
     );
 }
