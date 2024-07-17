3241b2b380064f4b5e2e1a463f25c6690403015e
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-17 18:39:19 -0400

    Restyle restaurant listings. Create functional routing.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import "./Header.scss"
 
 export default function Header () {
     return (
         <>
             <header>
-                <h2>
-                    SafeCuisine
-                </h2>
+                <Link to={"/"}>
+                    <h2>
+                        SafeCuisine
+                    </h2>
+                </Link>
             </header>
         </>
     );
--- client/src/components/RestaurantListItem/RestaurantListItem.jsx
+++ client/src/components/RestaurantListItem/RestaurantListItem.jsx
@@ -1,29 +1,22 @@
+import { Link } from "react-router-dom";
 import RatingsNumber from "../RatingsNumber/RatingsNumber";
 import "./RestaurantListItem.scss"
 
 export default function RestaurauntListItem () {
     return (
         <>
-        <div className="restauraunt-container">
-            <div className="rest-info-container">
-                <h1>Venable Rotisserie Bistro</h1>
-                <h3>
-                    <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">200 N Greensboro St A-18, Carrboro, NC 27510</a>
-                </h3>
-                <h3>1.32 miles</h3>
-            </div>
-            <RatingsNumber/>
-        </div>
-        <div className="restauraunt-container">
-            <div className="rest-info-container">
-                <h1>Buns</h1>
-                <h3>
-                    <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">200 N Greensboro St A-18, Carrboro, NC 27510</a>
-                </h3>
-                <h3>0.88 miles</h3>
-            </div>
-            <RatingsNumber/>
-        </div>
+            <Link to={"/restaurant/venableRotisserieBistro"}>
+                <div className="restauraunt-container">
+                    <div className="rest-info-container">
+                        <h1>Venable Rotisserie Bistro</h1>
+                        <h3>
+                            <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">200 N Greensboro St A-18, Carrboro, NC 27510</a>
+                        </h3>
+                        <h3>1.32 miles</h3>
+                    </div>
+                    <RatingsNumber/>
+                </div>
+            </Link>
         </>
     );
 }
