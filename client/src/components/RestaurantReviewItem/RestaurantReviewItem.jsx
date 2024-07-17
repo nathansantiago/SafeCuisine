4423d7a8a1254aed0bf8f506ed457c59f76f8492
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-17 13:30:38 -0400

    Create review item layout.

--- client/src/components/RestaurantReviewItem/RestaurantReviewItem.jsx
+++ client/src/components/RestaurantReviewItem/RestaurantReviewItem.jsx
@@ -1,9 +1,15 @@
+import RatingsStars from "../RatingsStars/RatingsStars";
 import "./RestaurantReviewItem.scss"
 
 export default function RestaurantReviewItem () {
     return (
-        <div>
-            This is a review
+        <div id="review-cont">
+            <div id="review-header">
+                
+            </div>
+            <RatingsStars/>
+            <p id="reviewed-for-allergens"></p>
+            <p id="review-text"></p>
         </div>
     );
 }
