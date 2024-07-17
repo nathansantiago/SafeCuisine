8739753e9a96e934e55372030b94a535f43363b1
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-17 18:18:42 -0400

    Style restaurant reviews.

--- client/src/components/RestaurantReviewItem/RestaurantReviewItem.jsx
+++ client/src/components/RestaurantReviewItem/RestaurantReviewItem.jsx
@@ -5,11 +5,12 @@ export default function RestaurantReviewItem () {
     return (
         <div id="review-cont">
             <div id="review-header">
-                
+                <p id="username">Jonathan</p>
+                <p id="review-date">Reviewed: 1/25/24</p>
             </div>
             <RatingsStars/>
-            <p id="reviewed-for-allergens"></p>
-            <p id="review-text"></p>
+            <p id="reviewed-for-allergens">Reviewed for: Gluten, Vegan</p>
+            <p id="review-text">Knowledgable Staff, no issues.</p>
         </div>
     );
 }
