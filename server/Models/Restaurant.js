a729ab430ee4475fb0feae332c21e612cb5a382d
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-20 09:27:37 -0400

    Create restaurant schema.

---
+++ server/Models/Restaurant.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Template for handling restaraunts in the database
+
+const RestaurantSchema = new Schema ({
+    address: {
+        building: String,
+        street: String,
+        coord: [Number],
+        zipcode: String,
+    },
+    menu: String,
+    name: String,
+    phone: String,
+});
+
+const RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+
+module.exports = RestaurantModel;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
+
+const Restaurant = require('./Models/Restaurant.js');
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(process.env.ATLAS_URI);
 
 // Test function to determine if the api is functioning properly.
 app.get('/', (req, res)=>{
     res.status(200);
     res.send("Welcome to root URL of Server");
 });
+
+// Gets restaurant data
+app.get('/restaurants', async (req,res) => {
+    res.json( await Restaurant.find() );
+})
 
 // Specifies what port the api should be listening on.
 app.listen(PORT, (error) => {
