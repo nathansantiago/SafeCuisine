67f836a0782da71296dd924f231f7e63d13ca1b7
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-20 23:22:45 -0400

    Create dynamic index page.

--- client/src/components/RestaurantListItem/RestaurantListItem.jsx
+++ client/src/components/RestaurantListItem/RestaurantListItem.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 import RatingsNumber from "../RatingsNumber/RatingsNumber";
 import "./RestaurantListItem.scss"
 
-export default function RestaurauntListItem () {
+export default function RestaurauntListItem ({ restaurantData }) {
     return (
         <>
-            <Link to={"/restaurant/669c71ea6a78518e9f6c1de5"}>
+            <Link to={"/restaurant/"+restaurantData._id}>
                 <div className="restauraunt-container">
                     <div className="rest-info-container">
-                        <h1>Venable Rotisserie Bistro</h1>
+                        <h1>{restaurantData.name}</h1>
                         <h3>
-                            <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">200 N Greensboro St A-18, Carrboro, NC 27510</a>
+                            <a href="https://maps.google.com/?q=200 N Greensboro St A-18, Carrboro, NC 27510">{restaurantData.address.building} {restaurantData.address.street}, {restaurantData.address.town}, {restaurantData.address.state} {restaurantData.address.zipcode}</a>
                         </h3>
                         <h3>1.32 miles</h3>
                     </div>
--- client/src/pages/IndexPage/IndexPage.jsx
+++ client/src/pages/IndexPage/IndexPage.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import RestaurauntListItem from "../../components/RestaurantListItem/RestaurantListItem";
 import "./IndexPage.scss"
+import axios from "axios";
 
 export default function IndexPage () {
+    const[restaurants, setRestaurants] = useState([]);
+    useEffect (() => {
+        axios.get('/restaurants').then(response => {
+            setRestaurants(response.data);
+        });
+    }, []);
+
     return (
         <div className="index-container">
-            <RestaurauntListItem/>
+            {/* <RestaurauntListItem restaurantData={restaurants[0]}/> */}
+            {restaurants.length > 0 && restaurants.map(restaurant => {
+                <RestaurauntListItem restaurantData={restaurant}/>
+            })}
         </div>
     );
 }
