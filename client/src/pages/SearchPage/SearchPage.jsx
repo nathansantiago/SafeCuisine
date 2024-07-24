6d001389b0c2414208bbca674aed4609b1d2da2a
Nathan Santiago <santiagonathan9254@gmail.com>
2024-07-24 12:33:17 -0400

    Create search form.

--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import { ArrowRightMagnifyingGlassIcon } from "../../components/Icons";
 import "./SearchPage.scss"
 
+import axios from "axios";
+
 export default function SearchPage () {
+    const[redirect, setRedirect] = useState(false);
+    const[location, setLocation] = useState(null);
+    const[distance, setDistance] = useState(0);
+    const[allergens, setAllergens] = useState([])
+
+    async function searchFormSubmit (ev, location, distance, allergens) {
+        ev.preventDefault();
+        
+        try {
+            const restaraunts = await axios.post('/search', {location, distance, allergens});
+            setRedirect(true);
+        }
+        catch (e) {
+            alert('search failed.');
+        }
+    }
+
     return (
         <div id="search-page">
             <div id="search-container">
-                <form action="">
+                <form onSubmit={searchFormSubmit}>
                     <label htmlFor="">Location (Zip or City):</label>
-                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" onChange={ev => setLocation(ev.target.value)}/>
                     <label htmlFor="">Distance in miles:</label>
-                    <input type="text" name="" id="" />
+                    <input type="number" name="" id="" onChange={ev => setDistance(ev.target.value)}/>
                     <label htmlFor="">Allergens:</label>
-                    <input type="text" />
+                    <div id="allergens-options">
+                        <input type="checkbox" />
+                        <label htmlFor="">Gluten</label>
+                        <input type="checkbox" />
+                        <label htmlFor="">Vegan</label>
+                    </div>
                     <button>
                         <ArrowRightMagnifyingGlassIcon/>
                     </button>
